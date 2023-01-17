899c17e084d51a0198baae99660ca985aecfb881
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2023-01-16 18:52:13 -0600

    corrected newItem()

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,19 @@
 
 
 //create add list item function
-function newItem(){
+function newItem() {
 
     let list = $('#list');
-    list.append('<li>list something</li>');
+    
 
-    let li = $('<li>list something</li>');
+    let li = $('<li></li>');
     list.append(li);
 
     let inputValue = $('#input').val();
     li.append(inputValue);
 
-
 }
+
+
+
+
