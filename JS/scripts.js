4ce53f1601bad14916f4ab6af8f040e705504825
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2023-01-16 20:18:10 -0600

    jquery toggle Strike class and sortable

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,18 +2,27 @@
 
 //create add list item function
 function newItem() {
-
+    //select html/node ID and assign to variable
     let list = $('#list');
-    
 
+    //create li variable and have it be an <li> element, append to list variable
     let li = $('<li></li>');
-    list.append(li);
+    list.append(li); 
 
+    //input variable -> value of input id/form -> append to li var
     let inputValue = $('#input').val();
     li.append(inputValue);
+    
+      
+    
+   let crossOut = li.on('dblclick', function() {
+    li.toggleClass('strike');
+});
+let crossOutButton = $(crossOutButton); 
+li.append(crossOutButton);
 
 }
 
 
 
-
+$('ol').sortable();
